12c7826e6eceeb4c8ae04e46b09961000cfd485c
DestroyerDarkNess <grupo.fisica.638@gmail.com>
2021-10-28 09:25:51 -0400

    no message

--- ConsoleHostDemo/Form1.Designer.vb
+++ ConsoleHostDemo/Form1.Designer.vb
@@ -26,13 +26,14 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.ConsoleHost1 = New ConsoleHostDemo.ConsoleHost()
+        Me.ConsoleHostV21 = New ConsoleHostDemo.ConsoleHostV2()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 444)
+        Me.Label1.Location = New System.Drawing.Point(3, 444)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(59, 13)
         Me.Label1.TabIndex = 1
@@ -42,12 +43,12 @@ Partial Class Form1
         '
         Me.TextBox1.Location = New System.Drawing.Point(68, 440)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(616, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(539, 20)
         Me.TextBox1.TabIndex = 2
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(690, 434)
+        Me.Button1.Location = New System.Drawing.Point(614, 437)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(70, 26)
         Me.Button1.TabIndex = 3
@@ -56,43 +57,53 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(766, 434)
+        Me.Button2.Location = New System.Drawing.Point(690, 437)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(80, 26)
         Me.Button2.TabIndex = 4
         Me.Button2.Text = "TestClolor"
         Me.Button2.UseVisualStyleBackColor = True
         '
-        'ConsoleHost1
+        'ConsoleHostV21
         '
-        Me.ConsoleHost1.BackColor = System.Drawing.Color.Black
-        Me.ConsoleHost1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.ConsoleHost1.ForeColor = System.Drawing.Color.White
-        Me.ConsoleHost1.Location = New System.Drawing.Point(0, 0)
-        Me.ConsoleHost1.Name = "ConsoleHost1"
-        Me.ConsoleHost1.Size = New System.Drawing.Size(858, 428)
-        Me.ConsoleHost1.TabIndex = 0
+        Me.ConsoleHostV21.BackColor = System.Drawing.Color.FromArgb(CType(CType(12, Byte), Integer), CType(CType(12, Byte), Integer), CType(CType(12, Byte), Integer))
+        Me.ConsoleHostV21.Dock = System.Windows.Forms.DockStyle.Top
+        Me.ConsoleHostV21.ForeColor = System.Drawing.Color.White
+        Me.ConsoleHostV21.Location = New System.Drawing.Point(0, 0)
+        Me.ConsoleHostV21.Name = "ConsoleHostV21"
+        Me.ConsoleHostV21.Size = New System.Drawing.Size(849, 428)
+        Me.ConsoleHostV21.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(776, 437)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(70, 26)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Matrix"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(858, 466)
+        Me.ClientSize = New System.Drawing.Size(849, 466)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ConsoleHostV21)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ConsoleHost1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents ConsoleHost1 As ConsoleHost
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents ConsoleHostV21 As ConsoleHostV2
+    Friend WithEvents Button3 As Button
 End Class
